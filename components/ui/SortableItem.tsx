dfc5c939d214b3dce92cf8379d78c0ae1117637f
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-03-02 10:59:40 -0600

    fixed delay and feedback styling.

--- components/ui/SortableItem.tsx
+++ components/ui/SortableItem.tsx
@@ -29,30 +29,28 @@ export default function SortableItem({
         transform: CSS.Transform.toString(transform),
         transition,
       }}
-      className={`flex my-2 ${
-        isDragging ? "outline outline-secondary" : "outline-none"
-      }  border-collapse rounded-sm`}
+      className={`flex my-2 outline-none ${
+        isDragging ? "outline-secondary" : ""
+      }  border-collapse rounded-sm bg-background`}
       aria-roledescription="sortable item"
     >
       <div
         {...attributes}
         {...listeners}
         className="grid items-center justify-center bg-secondary hover:bg-secondary mr-2 rounded-l-sm cursor-grab active:cursor-grabbing"
-        style={{ touchAction: "none" }}
+        style={{ touchAction: "manipulation" }}
         tabIndex={0}
         aria-label={`Drag handle for item ${id}`}
         aria-describedby={`drag-instructions-${id}`}
         role="button"
       >
-        <GripVertical className="w-4 h-4 sm:w-6 sm:h-6" />
+        <GripVertical className="w-6 h-6" />
         <span id={`drag-instructions-${id}`} className="sr-only">
           Press Space or Enter to activate drag mode, then use arrow keys to
           move, and Space or Enter again to drop
         </span>
       </div>
-      <div className="mr-2 flex-1" style={{ touchAction: "manipulation" }}>
-        {children}
-      </div>
+      <div className="mr-2 flex-1">{children}</div>
     </div>
   );
 }
